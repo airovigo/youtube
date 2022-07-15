d12a8677fedade7b622dff25494e7886491bd681
airovigo <106457208+airovigo@users.noreply.github.com>
2022-07-15 23:23:01 +0300

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 VIDEO=v.mp4
 
-set -e
+#set -e
+
+echo -n "youtube"
+    read youtube
 
 while true
 do
   ffmpeg -loglevel info -y -re \
-    -f -stream_loop -1 -i $VIDEO \
+    -re -stream_loop -1 -i $VIDEO \
     -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
     -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://$YOUTUBE_KEY
+    -f flv rtmp://$youtube
 done
